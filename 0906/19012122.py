a7da95062d5184a4fcd2676c4254687749901681
Julio Cesar Diaz <juliobory666@gmail.com>
2022-09-06 18:40:52 -0500

    julio cesar

--- 0906/19012122.py
+++ 0906/19012122.py
@@ -10,7 +10,7 @@ x= 22
 
 print("x      =    {:>6b}".format(x))
 print("x & 4  = {:>3d} = {:>6b}".format(x & 4, x & 4))
-print("x | 1  = {:>3d} = {:>6b}".format(x & 1, x | 1))
+print("x | 1  = {:>3d} = {:>6b}".format(x | 1, x | 1))
 print("x ^ 4  = {:>3d} = {:>6b}".format(x ^ 4, x ^ 4))
 print("~x     = {:>3d} = {:>6b}".format(~x , ~x))
 print("x << 1 = {:>3d} = {:>6b}".format(x << 1 , x << 1))
