3b86aa3d4f363aca6b70ae2a5d9dafb65bf93d64
Norika Isabel <za19011603@zapopan.tecmm.edu.mx>
2022-09-06 18:06:47 -0500

    OperadoresBits

---
+++ 0906/19011603.py
@@ -0,0 +1,15 @@
+
+# Nombre: Norika Isabel Ramirez Valdez 
+# No. Control: 19011603
+# Calificación: XXX
+
+x = 22
+
+print("x      = {:>6b}".format(x))
+print("x & 4  = {:>3d} = {:>6b}".format(x & 4, x & 4))
+print("x | 1  = {:>3d} = {:>6b}".format(x | 1, x | 1))
+print("x ^ 4  = {:>3d} = {:>6b}".format(x ^ 4, x ^ 4))
+print("~x     = {:>3d} = {:>6b}".format(~x, ~x))
+print("x << 1 = {:>3d} = {:>6b}".format(x << 1, x << 1))
+print("x >> 2 = {:>3d} = {:>6b}".format(x >> 2, x >> 2))
+
