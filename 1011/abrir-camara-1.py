76413c8bbee6e0ab651acd09dbe72ed13bc1c6b2
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2022-10-11 18:54:52 -0500

    ejemplo de camara

---
+++ 1011/abrir-camara-1.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+if not camara.isOpened():
+    print("No puedo abrir la camara")
+    exit(1)
+
+while True:
+    # Leemos la imagen de la camara
+    ret, imagen = camara.read()
+
+    if not ret:
+        print("No podemos capturar la imagen de la camara")
+        break
+
+    cv.imshow("Camara", imagen)
+
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()
