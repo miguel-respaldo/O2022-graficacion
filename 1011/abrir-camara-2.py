11b148a8f01a34841745361a70e3114ddcf9954f
KevinArmandoGonzalezBaeza <za19012313@zapopan.tecmm.edu.mx>
2022-11-08 18:24:29 -0600

    listo

--- 1011/abrir-camara-2.py
+++ 1011/abrir-camara-2.py
@@ -1,11 +1,19 @@
+from email.mime import image
 import cv2 as cv
+import os as o
+
+
+
+  
 
 camara = cv.VideoCapture(0)
+
+#
 
 if not camara.isOpened():
     print("No puedo abrir la camara")
     exit(1)
-
+contador = 1
 while True:
     # Leemos la imagen de la camara
     ret, imagen = camara.read()
@@ -14,16 +22,31 @@ while True:
         print("No podemos capturar la imagen de la camara")
         break
     imagenflip = cv.flip(imagen, 1)
+    gris = cv.cvtColor(imagenflip,cv.COLOR_BGR2GRAY,1)
     # 0 de cabeza
     # 1 espejo
     # -1 cabeza espejo
 
-    cv.imshow("Camara flip", imagenflip)
+    # cv.imshow("Camara flip", imagenflip)
     cv.imshow("Camara", imagen)
+    #cv.imshow("camara",gris)
 
+    t =cv.waitKey(1)
 
-    if cv.waitKey(1) == 27:
+    if  t == 27:
         break
+    elif t == ord('g'):
+        nombre = "imagen-{:05d}.png".format(contador)
+        print(f"Guardando imagen {nombre}")
+        cv.imwrite(nombre,gris)
+        contador += 1
+    elif t == ord('c'):
+        nombre = "imagen-{:05d}.png".format(contador)
+        print(f"Guardando imagen {nombre}")
+        cv.imwrite(nombre,imagen)
+        contador += 1
+    elif t  == ord('b'):
+        contador +=1
 
 camara.release()
 cv.destroyAllWindows()
--- 1018/guardar-imagen-2.py
+++ 1018/guardar-imagen-2.py
@@ -8,19 +8,53 @@ if not camara.isOpened():
 
 contador = 1
 en_color = True
+filtro1 = False 
+Gris = False
+Sobelx = False
+Sobely = False
+Laplician = False
+mouseX=0
+mouseY=0
+Pausar = True
+def draw_circle(event,x,y,flags,param):
+    global mouseX,mouseY
+    if event == cv.EVENT_LBUTTONDBLCLK:
+        cv.circle(imagen,(x,y),100,(66,41,36),2)
+        mouseX,mouseY = x,y
+
 while True:
     # Leemos la imagen de la camara
-    ret, imagen = camara.read()
+    if Pausar:
+        ret, imagen = camara.read()
 
     if not ret:
         print("No podemos capturar la imagen de la camara")
         break
 
-    if en_color:
-        cv.imshow("Camara", imagen)
-    else:
-        gris = cv.cvtColor(imagen, cv.COLOR_BGR2GRAY)
-        cv.imshow("Camara", gris)
+    cv.imshow("Camara", imagen)
+    cv.namedWindow('Camara')
+    cv.setMouseCallback('Camara',draw_circle)
+
+    if filtro1:
+        edge = cv.Canny(imagen, 100, 200)
+        cv.imshow('Camara', edge)
+   
+    if Gris:
+        gris=cv.cvtColor(imagen, cv.COLOR_BGR2GRAY)
+        cv.imshow('Camara',gris)
+
+    if Sobelx:
+       sobelx = cv.Sobel(imagen,cv.CV_64F,1,0,ksize=5)  # x
+       cv.imshow('Camara', sobelx)
+    if Sobely:
+        sobely = cv.Sobel(imagen,cv.CV_64F,0,1,ksize=5)  # y
+        cv.imshow('Camara', sobely)
+    if Laplician:
+        laplacian = cv.Laplacian(imagen,cv.CV_64F)
+        cv.imshow('Camara',laplacian)
+
+    
+
 
     teclado = cv.waitKey(1)
     if teclado == 27:
@@ -36,9 +70,22 @@ while True:
         print(f"Guardando imagen {nombre}")
         cv.imwrite(nombre,imagen)
         contador += 1
-    elif teclado == ord('b') or teclado == ord('B'):
-        en_color = not en_color
 
+    elif teclado == ord('b'):
+        Gris = not Gris
+
+    elif teclado == ord('1'):
+        filtro1 = not filtro1
+    elif teclado == ord('2'):
+        Sobelx = not Sobelx
+    elif teclado == ord('3'):
+        Sobely = not Sobely
+    elif teclado == ord('4'):
+        Laplician = not Laplician
+    elif teclado == ord('a'):
+        print (mouseX,mouseY)
+    elif teclado == ord('p'):
+        Pausar = not Pausar
 
 camara.release()
 cv.destroyAllWindows()
